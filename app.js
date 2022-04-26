d9de4f7d957e3989785cb7389d792107b696b62e
ernest <ernestas.du@gmail.com>
2022-04-26 23:11:02 +0100

    Player function added, started working on the game function

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 
-const choices = ['rock', 'paper', 'scissors'];
 
-function computerPlay(playerSelection, computerSelection) {
+const choices = ['rock', 'paper', 'scissors'];
+const computerChoice = computerPlay();
+
+// Computer plays rock, paper, scissors
+function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(computerPlay());
+
+//  Player plays rock, paper, scissors
+
+let playerChoice = prompt("Choose either Rock, Paper or Scissors").toLowerCase();
+
+function playRound(playerChoice, computerChoice) {
+    
+    if (playerChoice === computerChoice) {
+        return "Computer selected: " + computerChoice + "\n" + "You selected: " + playerChoice + "\n" + "It's a tie!";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        return "Computer selected: " + computerChoice + "\n" + "You selected: " + playerChoice + "\n" + "You lose!";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return "Computer selected: " + computerChoice + "\n" + "You selected: " + playerChoice + "\n" + "You win!";
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        return "Computer selected: " + computerChoice + "\n" + "You selected: " + playerChoice + "\n" + "You lose!";
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        return "Computer selected: " + computerChoice + "\n" + "You selected: " + playerChoice + "\n" + "You win!";
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        return "Computer selected: " + computerChoice + "\n" + "You selected: " + playerChoice + "\n" + "You lose!";
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        return "Computer selected: " + computerChoice + "\n" + "You selected: " + playerChoice + "\n" + "You win!";
+    } else {
+        return "You entered a wrong choice. Please restart the page, and try again.";
+    }
+}
+
+console.log(playRound(playerChoice, computerChoice));
+
+
+// Game function
+function game() {
+    for (let round = 0; round < 5; round++) {
+        playRound();
+        return whatRound = "Round: " + round;
+     }
+}
+
+
+console.log(game())
